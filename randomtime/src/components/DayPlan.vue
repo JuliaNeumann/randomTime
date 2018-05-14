025cd7733c3bc01d38c139c0ea834f54761fd353
JuliaNeumann <jn.julianeumann@gmail.com>
2018-05-14 13:59:54 +0200

    add vue project

---
+++ randomtime/src/components/DayPlan.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h1 class="uk-heading-divider">Get Day Plan</h1>
+        <form class="uk-form-stacked">
+            <fieldset class="uk-fieldset">
+                <label class="uk-form-label" for="number-of-hours">Number of hours to plan:</label>
+                <div class="uk-form-controls">
+                    <input class="uk-input uk-form-width-medium" id="number-of-hours" type="number">
+                </div>
+                <input type="submit" class="uk-button uk-button-primary uk-margin" value="Get">
+            </fieldset>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'DayPlan',
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+    };
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ randomtime/src/components/WeekPlan.vue
@@ -0,0 +1,32 @@
+<template>
+   <div>
+       <h1 class="uk-heading-divider">Create Week Plan</h1>
+       <form class="uk-form-stacked">
+           <fieldset class="uk-fieldset">
+               <label class="uk-form-label" for="number-of-hours">Number of hours to plan:</label>
+               <div class="uk-form-controls">
+                   <input class="uk-input uk-form-width-medium" id="number-of-hours" type="number">
+               </div>
+               <input type="submit" class="uk-button uk-button-primary uk-margin" value="Create">
+           </fieldset>
+       </form>
+   </div>
+</template>
+
+<script>
+
+const randomTime = require("./../randomTime");
+
+export default {
+  name: 'WeekPlan',
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+    };
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
