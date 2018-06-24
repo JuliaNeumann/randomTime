0f7d9378b7ee043c78a7c738cf2a1f27c175ef4a
JuliaNeumann <jn.julianeumann@gmail.com>
2018-06-24 15:59:48 +0200

    adjust config for openshift

--- app/index.js
+++ app/index.js
@@ -2,12 +2,14 @@
 const express = require("express");
 const config = require("./config");
 const Router = require("./router");
+const server_port = process.env.OPENSHIFT_NODEJS_PORT || 9000;
+const server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
 let app = express();
 
 Router.init(app, config);
 
-app.listen(config.port, () => {
+app.listen(server_port, server_ip_address, () => {
     // tslint:disable-next-line:no-console
-    console.log(`Worker ${process.pid} running a ${config.env} server listening on port ${config.port}`);
+    console.log(`Worker ${process.pid} running a ${config.env} server listening on port ${server_port}`);
 });
