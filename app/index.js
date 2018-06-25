b72b5ce0b8c3b4ee999eb113e33418ce7df60279
JuliaNeumann <jn.julianeumann@gmail.com>
2018-06-25 20:19:30 +0200

    fix port config

--- app/index.js
+++ app/index.js
@@ -2,14 +2,22 @@
 const express = require("express");
 const config = require("./config");
 const Router = require("./router");
-const server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+const server_port = process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 8080;
 const server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 let app = express();
 
 Router.init(app, config);
 
-app.listen(server_port, server_ip_address, () => {
+if (process.env.OPENSHIFT_NODEJS_PORT) {
+  app.listen(server_port, server_ip_address, () => {
     // tslint:disable-next-line:no-console
     console.log(`Worker ${process.pid} running a ${config.env} server listening on port ${server_port}`);
-});
+  });
+}
+else {
+  app.listen(server_port, () => {
+    // tslint:disable-next-line:no-console
+    console.log(`Worker ${process.pid} running a ${config.env} server listening on port ${server_port}`);
+  });
+}
