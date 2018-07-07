90c93d9453bbdc7e8434459b044de327d8b61645
JuliaNeumann <jn.julianeumann@gmail.com>
2018-07-07 19:27:43 +0200

    set up unit testing

---
+++ test/main.test.js
@@ -0,0 +1,12 @@
+import { mount } from 'vue-test-utils'
+import Main from '../app/routes/main/main.vue'
+
+describe('Main', () => {
+  const wrapper = mount(Main);
+
+  it('renders the correct markup', () => {
+    expect(wrapper.contains('h1')).toBe(true);
+    expect(wrapper.contains('p')).toBe(true);
+  })
+
+});
