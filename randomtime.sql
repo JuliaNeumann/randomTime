e408915da665841a7eac1f35c05c7e566008a764
JuliaNeumann <jn.julianeumann@gmail.com>
2018-10-13 21:27:06 +0200

    adapt sql

--- randomtime.sql
+++ randomtime.sql
@@ -1,6 +1,4 @@
 /* psql -h localhost -p 5433 -d randomTime -U randomTime --password */
-
+DROP TABLE IF EXISTS weekplan;
 CREATE TABLE weekplan (id INT PRIMARY KEY, author VARCHAR(50), plan TEXT, config TEXT, password VARCHAR(50));
-INSERT INTO weekplan (id, author) VALUES (1, 'Jule');
-INSERT INTO weekplan (id, author) VALUES (2, 'Jonas');
-INSERT INTO weekplan (id, author) VALUES (3, 'Anne');
+INSERT INTO weekplan (id, author) VALUES (1, 'John Doe');
